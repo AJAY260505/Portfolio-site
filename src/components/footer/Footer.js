0773b703bb58f0e4bc0c5f1f2675a9afde82b8a5
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2024-10-20 22:46:54 +0530

    footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="w-full py-20 h-auto border-b-[1px] border-b-black flex flex-col items-center">
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/footer/FooterBottom.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterBottom = () => {
+  return (
+    <div className="py-10 w-full">
+      <p className="text-base text-center text-gray-500">
+       
+      </p>
+    </div>
+  );
+}
+
+export default FooterBottom
